19ab61e172a0c88782a0603dbbe898b7c1b10ae2
LAPTOP-CE9FDRAL\Eigenaar <kasper.dejans@student.kuleuven.be>
2022-03-14 16:05:26 +0100

    Nieuw

--- model/Minesweeper.java
+++ model/Minesweeper.java
@@ -1,33 +1,65 @@
 package model;
 
-import static model.Difficulty.EASY;
-import static model.Difficulty.MEDIUM;
+import static model.Difficulty.*;
 
 public class Minesweeper extends AbstractMineSweeper{
-    int mines;
-    int wBords;
-    int hBords;
+    int explosionCount;
+    int col;
+    int row;
+    Difficulty level;
+
 
     @Override
     public int getWidth() {
-        return 0;
+        return col;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return row;
     }
 
     @Override
     public void startNewGame(Difficulty level) {
-
+    setDifficulty(level);
     }
 
     @Override
     public void startNewGame(int row, int col, int explosionCount) {
-
+    setDifficulty(this.level);
     }
 
+    public void setDifficulty(Difficulty level)
+    {
+        this.level = level;
+        if(this.level== EASY  )
+        {
+            this.explosionCount = 10;
+            this.col = 8;
+            this.row = 8;
+
+        }
+        else if(this.level== MEDIUM)
+        {
+            this.explosionCount = 40;
+            this.col = 16;
+            this.row = 16;
+        }
+        else if(this.level== HARD)
+        {
+            this.explosionCount = 40;
+            this.col = 16;
+            this.row = 30;
+        }
+        else
+        {
+            this.level = EASY;
+            this.explosionCount = 10;
+            this.col = 8;
+            this.row = 8;
+        }
+
+    }
     @Override
     public void toggleFlag(int x, int y) {
 
